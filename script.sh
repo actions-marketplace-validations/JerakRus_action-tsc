34ab86d408b6d8e3832c477447217c21685494d1
Aleksey Stroitelev <35634599+JerakRus@users.noreply.github.com>
2023-04-06 18:03:58 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,23 +4,23 @@ cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ ! -f "$(npm root)"/.bin/tsc ]; then
-  echo "::group::🔄 Running npm install to install tsc ..."
-  npm install
+if [ ! -f /node_modules/.bin/tsc ]; then
+  echo "::group::🔄 Running yarn install to install tsc ..."
+  yarn install
   echo "::endgroup::"
 fi
 
-if [ ! -f "$(npm root)"/.bin/tsc ]; then
+if [ ! -f /node_modules/.bin/tsc ]; then
   echo "❌ Unable to locate or install tsc. Did you provide a workdir which contains a valid package.json?"
   exit 1
 else
 
-  echo ℹ️ tsc version: "$("$(npm root)"/.bin/tsc --version)"
+  echo ℹ️ tsc version: "$(/node_modules/.bin/tsc --version)"
 
   echo "::group::📝 Running tsc with reviewdog 🐶 ..."
 
   # shellcheck disable=SC2086
-  "$(npm root)"/.bin/tsc ${INPUT_TSC_FLAGS} |
+  /node_modules/.bin/tsc ${INPUT_TSC_FLAGS} |
     reviewdog -f=tsc \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER}" \
